174f1d15bd4ed8c85765e64fbba6c3c5050e59c2
May Steinfeld <maylevsteinfeld@gmail.com>
2020-06-21 16:05:15 +0300

    cp2 add more sleep time

--- scraping_glassdoor.py
+++ scraping_glassdoor.py
@@ -9,19 +9,22 @@ from datetime import date
 import pandas as pd
 import random
 from selenium import webdriver  # allows us to open a browser and do the navigation
+from selenium.webdriver.support.ui import WebDriverWait
 
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 
 # Global variables
-SLEEP_TIME = random.randint(5,10)  # Random number for time sleep, depends on computers- in our we meed minimum 5
+SLEEP_TIME = 10  # Random number for time sleep, depends on computers- in our we meed minimum 5
 URL = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword' \
       '=&locT=N&locId=119&jobType=&context=Jobs&sc.keyword=&dropdown=0 '
 
+# TODO: put all webdriver part inside a function
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')  # scrape without a new Chrome window every time
-options.add_argument('enable-features=NetworkServiceInProcess')
-options.add_argument("disable-features=NetworkService")
+#options.add_argument('enable-features=NetworkServiceInProcess')
+#options.add_argument("disable-features=NetworkService")
 #options.add_argument('start-maximized')
+#options.add_argument("--lang=en-US") #forces the English interface
 
 browser = webdriver.Chrome(chrome_options=options)
 browser.maximize_window()
@@ -53,6 +56,7 @@ def get_number_of_jobs():
         num_of_available_jobs = 1000
         print(f'Their is a problem trying to get the number of available jobs post, by default the number of '
               f'available jobs post to scrap will be {num_of_available_jobs}')
+    print(num_of_available_jobs)
     return num_of_available_jobs
 
 
@@ -65,7 +69,7 @@ def collecting_data(num_of_jobs):
     """
 
     while len(jobs_list) < num_of_jobs:
-
+        print(len(jobs_list))
         time.sleep(SLEEP_TIME)  # Wait until the page load
 
         try:
@@ -73,7 +77,7 @@ def collecting_data(num_of_jobs):
         except ElementClickInterceptedException:
             pass
 
-        time.sleep(SLEEP_TIME)
+        #time.sleep(SLEEP_TIME)
 
         # Click to the X to close popups
         try:
@@ -92,6 +96,7 @@ def collecting_data(num_of_jobs):
                 break
 
             button_job.click()
+            # start collect job data
 
             # Catch the publication date
             job_age = browser.find_element_by_xpath("//li[contains(@class,'selected')]//div["
@@ -110,33 +115,34 @@ def collecting_data(num_of_jobs):
 
             time.sleep(SLEEP_TIME)
 
-            # Collect Company Name from a post
-            company_name = browser.find_element_by_xpath('//div[@class="employerName"]').text
+            # Collect mandatory information
+            collect_mandatory = False
+            while not collect_mandatory:
+                try:
+                    # Collect Company Name from a post
+                    company_name = browser.find_element_by_xpath('//div[@class="employerName"]').text
+                    # Collect Job Title from a post
+                    job_title = browser.find_element_by_xpath('//div[@class="title"]').text
+                    # Collect Job Location from a post
+                    job_location = browser.find_element_by_xpath('//div[@class="location"]').text
+                    # Collect Job Description
+                    job_description = browser.find_element_by_xpath('//div[@class="jobDescriptionContent desc"]').text
 
-            # Sometimes, company name and rating are join, we need to split them into Company Name and Rating
-            # information.
+                    # Sometimes, czompany name and rating are join, we need to split them into Company Name and Rating
+                    # information.
 
-            if '\n' in company_name:  # We have a rating
-                rating = company_name.split('\n')[1]
-                company_name = company_name.split('\n')[0]
-            else:  # No rating
-                rating = None
+                    if '\n' in company_name:  # We have a rating
+                        rating = company_name.split('\n')[1]
+                        company_name = company_name.split('\n')[0]
+                    else:  # No rating
+                        rating = None
 
-            # Collect mandatory information (title + location)
-            # Collect Job Title from a post
-            job_title = browser.find_element_by_xpath('//div[@class="title"]').text
-
-            # Collect Job Location from a post
-            job_location = browser.find_element_by_xpath('//div[@class="location"]').text
+                    collect_mandatory = True
+                except NoSuchElementException:
+                    #button_job.click()
+                    time.sleep(SLEEP_TIME)
 
             # Collect optional information
-
-            try:
-                # Collect Job Description
-                job_description = browser.find_element_by_xpath('//div[@class="jobDescriptionContent desc"]').text
-            except NoSuchElementException:
-                job_description = None
-
             try:
                 # Click on company in the hyper details
                 browser.find_element_by_xpath('//div[@class="tab" and @data-tab-type="overview"]').click()
@@ -217,15 +223,25 @@ def collecting_data(num_of_jobs):
                               "sector": company_sector, "type": company_type, "rating": rating,
                               "competitors": company_competitors, "revenue": company_revenue,
                               "Headquarters": company_headquarters})
+
         # save each page to the csv file in case glassdoor block us
         create_csv_file(jobs_list)
         # Clicking on the "next page" button if finished collected all jobs post from current page
         if len(jobs_list) < num_of_jobs:
+            print("Enter if")
             try:
-                browser.find_element_by_xpath('.//li[@class="next"]//a').click()
+                #problem_next = WebDriverWait(browser, 15).until(lambda browser: browser.find_elements_by_xpath('//li[@class="next"]//a[@data-test="pagination-next"]').click())
+                problem_next = browser.find_element_by_xpath('//li[@class="next"]//a[@data-test="pagination-next"]').click()
+                print(problem_next)
+                print("Find next")
             except NoSuchElementException:
                 print(f"Finished, no more jobs than{len(jobs_list)} in the website")
                 break
+        else:
+            print("Finish jobs error")
+
+        # wait until catch the job posts in the next page
+        #WebDriverWait(browser, 15).until(lambda browser: browser.find_elements_by_xpath("//li[contains(""@class, ""'job-listing')]"))
 
 
 if __name__ == "__main__":
