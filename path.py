608025ea7fa186d85b7afb43b5b5b4a165bcd4a0
May Steinfeld <maylevsteinfeld@gmail.com>
2020-07-02 11:13:51 +0300

    change path error

--- path.py
+++ path.py
@@ -1,4 +1,4 @@
-DEFAULT_URL = f'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=&locT=N&locId=119&jobType=&context=Jobs&sc.keyword=&dropdown=0
+DEFAULT_URL = f'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=&locT=N&locId=119&jobType=&context=Jobs&sc.keyword=&dropdown=0'
 EXE_PATH = r'/Users/Sheryl/PycharmProjects/Itc_data_mining/webDrivers/geckodriver'
 pop_up_xpath = "//div[contains(@class,'modal_main')]//span[@class='SVGInline modal_closeIcon']"
 num_pages_xpath = "//div[@class='cell middle hideMob padVertSm']"
