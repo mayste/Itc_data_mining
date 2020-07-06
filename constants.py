299db8b51523e0a70db4e66b9c5f873130806c3e
May Steinfeld <maylevsteinfeld@gmail.com>
2020-07-06 22:43:19 +0300

    add to do

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import random
 
-# TODO: Change to dict of dict
+# TODO: Change one file of constants + xpath + etc and one file for sql queries + one file for a text_msgs
 # Constant
 SLEEP_TIME = random.randint(5, 10)
 LAST_ELEMENT = -1
