24eee885a9ad5b952fc40508a5020b140b9dcdfc
Sheryl Sitruk <sitruksh@gmail.com>
2020-07-29 10:29:42 +0300

    Return previous version

--- scraper.py
+++ scraper.py
@@ -15,9 +15,10 @@ class Scraper:
         """
         Sets up the default URL.
         """
-        options = Options()
-        options.headless = True
-        self.browser = webdriver.Firefox(executable_path=driver_path, firefox_options=options)
+        # options = Options()
+        # options.headless = True
+        # self.browser = webdriver.Firefox(executable_path=driver_path, firefox_options=options)
+        self.browser = webdriver.Firefox(executable_path=driver_path)
         # chrome_options = Options()
         # chrome_options.add_argument('--headless')
         # chrome_options.add_argument('--no-sandbox')
